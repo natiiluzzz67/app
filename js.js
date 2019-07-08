32903efae8fd1cfb552ca7bf7d5e14cc772f3f27
isadoraroman <48677139+isadoraroman@users.noreply.github.com>
2019-07-08 08:45:29 -0300

    Adicionei um quarto objeto no app

--- js.js
+++ js.js
@@ -18,11 +18,22 @@ let filme3 =
     classificacao: "14",
     diretor: "Gary Dauberman"
 }
+let filme4 = 
+{
+    name: "10 coisas que eu odeio em você",
+    classificacao: "10",
+    diretor: "Gil Junger"
+}
+
 
 
 cinema.push(filme1)
 cinema.push(filme2)
 cinema.push(filme3)
+cinema.push(filme4)
+
+
+
 
 
 console.log(cinema)
