a6fe0d8742c90067bf93f8b3c5f7c4d420b82c28
isadoraroman <48677139+isadoraroman@users.noreply.github.com>
2019-07-08 08:44:20 -0300

    Adicionei mais um objeto na lista

--- js.js
+++ js.js
@@ -12,11 +12,18 @@ let filme2 =
     classificacao: "livre",
     diretor: "Josh Cooley"
 }
-
+let filme3 = 
+{
+    name: "Annabelle",
+    classificacao: "14",
+    diretor: "Gary Dauberman"
+}
 
 
 cinema.push(filme1)
 cinema.push(filme2)
- 
+cinema.push(filme3)
+
+
 console.log(cinema)
 
