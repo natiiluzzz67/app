80054d8d6ccd7d59f897ab04ea92ba33f2e5e341
natiiluzzz67 <48677297+natiiluzzz67@users.noreply.github.com>
2019-07-08 08:29:25 -0300

    iniciamos a lista de objetos

---
+++ js.js
@@ -0,0 +1,22 @@
+cinema = []
+
+let filme1 = 
+{
+    name: "Aladdin",
+    classificacao: "livre",
+    diretor: "Guy Ritchie"
+}
+let filme2 = 
+{
+    name: "Toy Story 4",
+    classificacao: "livre",
+    diretor: "Josh Cooley"
+}
+
+
+
+cinema.push(filme1)
+cinema.push(filme2)
+ 
+console.log(cinema)
+
